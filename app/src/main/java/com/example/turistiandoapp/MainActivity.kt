85578e9f3a034825bf456836d25f3fca1c88cdbd
David2813 <quinterosotodavidjavier@gmail.com>
2020-10-26 00:04:05 -0500

    MainAppTuristeando parte 1

--- app/src/main/java/com/example/turistiandoapp/MainActivity.kt
+++ app/src/main/java/com/example/turistiandoapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.turistiandoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.*
 import androidx.core.text.set
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         val act = (findViewById(R.id.textNomActividad) as EditText).text
         val dire = (findViewById(R.id.textDirecccion) as EditText).text
         val desc = (findViewById(R.id.textDescripcion) as EditText).text
-        var costo = (findViewById(R.id.textCosto) as EditText).text
+        val costo = (findViewById(R.id.textCosto) as EditText).text
         val btn = findViewById(R.id.btnPublicar) as Button
         var nomMun :String = ""
         var mun = (findViewById(R.id.spinner) as Spinner)
@@ -33,8 +34,25 @@ class MainActivity : AppCompatActivity() {
             }
         }
         btn.setOnClickListener{
+            var numero = true
             val costo2 = costo.toString().toInt()
-            database.child(act.toString()).setValue(Publicaciones(act.toString(),nomMun,dire.toString(),desc.toString(),costo2))
+            if(TextUtils.isEmpty(act)){
+                Toast.makeText(applicationContext,"Llena el campo de actividad",Toast.LENGTH_LONG).show()
+            }else if(TextUtils.isEmpty(dire)){
+                Toast.makeText(applicationContext,"Llena el campo de dirección",Toast.LENGTH_LONG).show()
+            }else if (TextUtils.isEmpty(desc)){
+                Toast.makeText(applicationContext,"Llena el campo de descripción",Toast.LENGTH_LONG).show()
+            }else if(TextUtils.isEmpty(costo)){
+                Toast.makeText(applicationContext,"Llena el campo de costo",Toast.LENGTH_LONG).show()
+            }else if((TextUtils.isEmpty(dire))&&(TextUtils.isEmpty(act))&&(TextUtils.isEmpty(desc))&&(TextUtils.isEmpty(costo))){
+                Toast.makeText(applicationContext,"Falta algún dato",Toast.LENGTH_LONG).show()
+            }else{
+                numero = costo.matches("-?\\d+(\\.\\d+)?".toRegex())
+                if (numero)
+                    database.child(act.toString()).setValue(Publicaciones(act.toString(),nomMun,dire.toString(),desc.toString(),costo2))
+                else
+                    Toast.makeText(applicationContext,"El costo debe ser un dato de tipo numerico",Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
