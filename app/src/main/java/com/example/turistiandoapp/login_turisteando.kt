fe00f3df0b704412354d0d03ea8c334afb27c68e
David2813 <quinterosotodavidjavier@gmail.com>
2020-10-27 10:43:36 -0500

    Creación actividades login y registro

---
+++ app/src/main/java/com/example/turistiandoapp/login_turisteando.kt
@@ -0,0 +1,11 @@
+package com.example.turistiandoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_turisteando : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_turisteando)
+    }
+}
