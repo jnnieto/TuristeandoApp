85ef53a314081210b0afa91c5916c43ee54b2daa
Sebastian Naranjo <42097936+sebsnaranjo@users.noreply.github.com>
2020-10-26 23:29:34 -0500

    Inicio de la aplicion y reubicacion del splash screen.

--- app/src/main/java/com/example/turistiandoapp/InicioTuristeando.kt
+++ app/src/main/java/com/example/turistiandoapp/InicioTuristeando.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class InicioTuristeando : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_TuristiandoApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_turisteando)
     }
